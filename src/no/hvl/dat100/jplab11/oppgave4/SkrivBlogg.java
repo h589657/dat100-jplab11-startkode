1950d7cbf37eb767cb60637a248492945878da19
h589657 <69852641+h589657@users.noreply.github.com>
2020-11-12 10:55:19 +0100

    Add files via upload

--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -3,13 +3,26 @@ package no.hvl.dat100.jplab11.oppgave4;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
-import no.hvl.dat100.jplab11.common.TODO;
 import no.hvl.dat100.jplab11.oppgave3.*;
 
 public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
 
-		throw new UnsupportedOperationException(TODO.method());
+		boolean sattInn = false;
+		PrintWriter skriver = null;
+		try {
+			skriver = new PrintWriter(mappe+filnavn);
+			sattInn = true;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return sattInn;
+		}
+		skriver.print(samling.getAntall() + "\n");
+		for(int i = 0; i < samling.getAntall(); i++) {
+		skriver.print(samling.getSamling()[i].toString());
+		}
+		skriver.close();
+		return sattInn;
 	}
 }
